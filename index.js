9e9f77788b71dbc6a5a9612c5b91ed7d5734d617
Atik Hassan <atikhassant64@gmail.com>
2025-12-11 22:49:09 +0600

    added tickets get api by id

--- index.js
+++ index.js
@@ -24,6 +24,32 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const db = client.db("ticket_bari");
+        const ticketsCollection = db.collection("tickets");
+
+        // tickets post Api
+        app.post("/tickets", async (req, res) => {
+            const tickets = req.body;
+            const result = await ticketsCollection.insertOne(tickets);
+            res.send(result);
+        })
+
+        // All tickets get Api
+        app.get("/tickets", async (req, res) => {
+            const cursor = ticketsCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // tickets get api by id
+        app.get("/tickets/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await ticketsCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");

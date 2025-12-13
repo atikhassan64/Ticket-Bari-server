e12f4fe0b5f6287c6c5588273d6da8f51ef18bc2
Atik Hassan <atikhassant64@gmail.com>
2025-12-13 22:44:42 +0600

    added my booked tickets post api

--- index.js
+++ index.js
@@ -26,7 +26,24 @@ async function run() {
         await client.connect();
 
         const db = client.db("ticket_bari");
+        const userCollection = db.collection("users");
         const ticketsCollection = db.collection("tickets");
+        const bookedTicketCollection = db.collection("bookedTickets");
+
+        // user post related Api
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            user.role = "user";
+            user.createAt = new Date();
+            const email = user.email;
+            const userExists = await userCollection.findOne({ email });
+            if (userExists) {
+                return res.send({ message: "user exists" })
+            }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
 
         // tickets post Api
         app.post("/tickets", async (req, res) => {
@@ -34,13 +51,6 @@ async function run() {
             const result = await ticketsCollection.insertOne(tickets);
             res.send(result);
         })
-
-        // // All tickets get Api
-        // app.get("/tickets", async (req, res) => {
-        //     const cursor = ticketsCollection.find({});
-        //     const result = await cursor.toArray();
-        //     res.send(result);
-        // })
 
         // tickets get api by email
         app.get("/tickets", async (req, res) => {
@@ -70,6 +80,37 @@ async function run() {
             const result = await ticketsCollection.deleteOne(query);
             res.send(result)
         })
+
+        // tickets update api by id
+        app.patch('/transactions/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateTransaction = req.body;
+            const query = { _id: new ObjectId(id) };
+            const update = {
+                $set: {
+                    type: updateTransaction.type,
+                    category: updateTransaction.category,
+                    amount: updateTransaction.amount,
+                    description: updateTransaction.description,
+                    date: updateTransaction.date
+                }
+            }
+            const options = {};
+            const result = await finEaseCollection.updateOne(query, update, options);
+            res.send(result);
+        })
+
+        // my booked tickets post Api
+        app.post("/ticket-booked", async (req, res) => {
+            const bookedTicket = req.body;
+            bookedTicket.status = "pending";
+            bookedTicket.createAt = new Date();
+            const result = await bookedTicketCollection.insertOne(bookedTicket);
+            res.send(result)
+        })
+
+        // my booked tickets get api
+        
 
 
         // Send a ping to confirm a successful connection
